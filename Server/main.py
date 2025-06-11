0b9876bfa1eb701c1d16953a31fa19f9c45faf20
rahul <rg4005450@gmail.com>
2025-06-11 21:21:48 +0530

    fiel structure changed main app working fine

--- Server/main.py
+++ Server/main.py
@@ -1,67 +1,56 @@
-from fastapi import FastAPI ,WebSocket, pydantic
-from typing import Dict ,List
-import json 
-import uuid
-app = FastAPI() 
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+from typing import Dict, List
+import json
 
+app = FastAPI()
 
+class RoomManager:
+    def __init__(self):
+        self.rooms: Dict[str, List[WebSocket]] = {}
 
-    
-       
-       
-            
-class Room:
-    rooms : Dict[str , List[WebSocket]]
-    
+    async def connect(self, socket: WebSocket, room_id: str):
+        if room_id not in self.rooms:
+            self.rooms[room_id] = []
+        self.rooms[room_id].append(socket)
 
-class RoomManager : 
-    # make the dict
-    def __init__(self): 
-      self.rooms :Dict[str , List[WebSocket]] = {} 
-  
-    def Connect(self, soc,room_id): 
-        
-        if room_id not in self.rooms: 
-         self.rooms[room_id] = [] 
-        self.rooms[room_id].append(soc); 
-        
+    async def broadcast(self, room_id: str, message: str):
+        room = self.rooms.get(room_id, [])
+        for socket in room:
+            await socket.send_text(message)
 
-    def broadcast(self ,room_id,message):
-        room = self.rooms[room_id]
-        for soc in room : 
-            soc.send_message(message)
-    
-    def messageHandler(self, message: dict,room_id):
+    async def handle_message(self, message: dict, room_id: str):
         message_type = message.get('type')
-        if message_type == 'offer': 
-            self.broadcast(room_id,message.get('sdp'))
-        elif message_type == 'answer' : 
-            self.broadcast(room_id,message.get('sdp'))
-        elif message_type == 'sdp': 
-            self.broadcast(room_id,message.get('sdp'))
-        elif message_type == 'leave-room': 
-            self.leaveRoom(room_id,message.get('soc'))
-           
-    def leaveRoom(self, room_id,leaving_socket):
-        #for key in self.rooms:
-        if room_id in self.rooms: 
-            self.rooms[room_id].remove(soc)
-            if self.rooms[room_id].count() == 0:
+        msg = json.dumps(message)
+
+        if message_type in ['offer', 'answer', 'sdp']:
+            await self.broadcast(room_id, msg)
+        elif message_type == 'leave-room':
+            await self.leave_room(room_id, message.get('socket'))
+
+    async def leave_room(self, room_id: str, leaving_socket: WebSocket):
+        if room_id in self.rooms:
+            if leaving_socket in self.rooms[room_id]:
+                self.rooms[room_id].remove(leaving_socket)
+            if len(self.rooms[room_id]) == 0:
                 del self.rooms[room_id]
-                
-    
 
-            
-Manager = RoomManager()
+manager = RoomManager()
 
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
 
-@app.websocket('/ws')
-async def soc(websoc ):
-    await  websoc.accept()
-    while(True):
-        res= websoc.recieve_text()
-        if res ==None: 
-            print('messaeg is empty')
-            continue
-        res_json = json.loads(res)
-        Manager.Connect(websoc, res_json['room_id'])
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    try:
+        while True:
+            res = await websocket.receive_text()
+            if not res:
+                continue
+            res_json = json.loads(res)
+            room_id = res_json.get("room_id")
+            await manager.connect(websocket, room_id)
+            await manager.handle_message(res_json, room_id)
+    except WebSocketDisconnect:
+        print("Client disconnected")
